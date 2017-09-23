321fe0344bc54caa9da21685d3a851c142d5fc68
juanpa10 <jp.camacho10@uniandes.edu.co>
2017-09-22 23:43:01 -0500

    arreglo de app.js solo para el deploy

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   //next(err);
-  res.sendFile(path.resolve(__dirname, 'front/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname,'..', 'front/build', 'index.html'));
 });
 
 // error handler
