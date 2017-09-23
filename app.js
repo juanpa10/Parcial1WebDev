febf24e4e33a264b974890414481e1b1b9d3db3e
juanpa10 <jp.camacho10@uniandes.edu.co>
2017-09-22 23:46:58 -0500

    arreglo de app.js solo para el deploy

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'front/build')));
+app.use(express.static(path.join(__dirname, './front/build')));
 
 app.use('/', index);
 app.use('/users', users);
@@ -30,7 +30,7 @@ app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   //next(err);
-  res.sendFile(path.resolve(__dirname,'..', 'front/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './front/build', 'index.html'));
 });
 
 // error handler
